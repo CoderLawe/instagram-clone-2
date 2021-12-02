cdc65c6a2e8c88c66879fea3e752e6d141f63748
CoderLawe <52962217+CoderLawe@users.noreply.github.com>
2021-12-02 04:26:01 +0200

    fixing auth bug

--- components/Story.js
+++ components/Story.js
@@ -1,7 +1,7 @@
 function Story({ img, username}) {
     return (
         <div>
-            <img className="h-14 w-14 rounded-full p-[1.5px]  border-red-500 border-2 object-contain hover:scale-110 transform transition-al duration-300 ease-out cursor-pointer"src="https://avatars.githubusercontent.com/u/52962217?v=4" alt=""/>
+            <img className="h-14 w-14 rounded-full p-[1.5px]  border-red-500 border-2 object-contain hover:scale-110 transform transition-al duration-300 ease-out cursor-pointer"src={img} alt=""/>
             <p className="text-xs w-14 truncate text-center">{username}</p>
         </div>
     )
